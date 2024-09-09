32417dddbab53149d6b5e69596cff11ddc44127b
Hell-Vamp <00001119142167sp@al.educacao.sp.gov.br>
2024-09-09 07:34:40 -0300

    add sketch & style

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let cor;
+let circuloX;
+let circuloY;
+
+function setup() {
+  createCanvas(400, 400);
+  background(color(0, 0, 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 255));
+  circuloX = [0, 0, 0];
+  circuloY = [random(height), random(height), random(height)];
+  
+  textFont("Comic Sans MS");
+  textSize(30)
+  textAlign(CENTER,CENTER);
+}
+
+function draw() {
+  
+  fill(cor);
+  
+  text("Aprendendo JS na Alura",200,200)
+  
+  for(let contador in circuloX) {
+      circle(circuloX[contador], circuloY[contador], 50);
+      circuloX[contador] += random(0, 3);
+      circuloY[contador] += random(-3, 3);
+    
+    if (circuloX[contador] >= width) {
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+    }
+  }
+  
+  if (mouseIsPressed) {
+      cor = color(random(0, 255), random(0, 255), random(0, 255));
+  }
+  
+}
